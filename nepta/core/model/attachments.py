7ddd34052909b90c5a585ab7c4c7659a288c0959
Adrian Tomasov <atomasov@redhat.com>
2019-08-01 12:36:11 +0200

    [attch] adding alias option

--- nepta/core/model/attachments.py
+++ nepta/core/model/attachments.py
@@ -1,26 +1,32 @@
 class GenericAttachment(object):
+    def __init__(self, alias=None):
+        self.alias = alias
+
     def __str__(self):
-        return f'{self.__class__.__name__} attachments: {self.__dict__}'
+        return f'{self.__class__.__name__} attachment: {self.__dict__}'
 
 
 class File(GenericAttachment):
-    def __init__(self, file_path):
+    def __init__(self, file_path, alias=None):
+        super().__init__(alias)
         self.f_path = file_path
 
 
 class Directory(GenericAttachment):
-    def __init__(self, directory_path):
+    def __init__(self, directory_path, alias=None):
+        super().__init__(alias)
         self.d_path = directory_path
 
 
 class Command(GenericAttachment):
-    def __init__(self, cmdline):
+    def __init__(self, cmdline, alias=None):
+        super().__init__(alias)
         self.cmdline = cmdline
 
 
 class CycleCommand(Command):
-    def __init__(self, cmd_for_list, cmdline):
-        super().__init__(cmdline)
+    def __init__(self, cmd_for_list, cmdline, alias=None):
+        super().__init__(cmdline, alias)
         self.cmd_for_list = cmd_for_list
 
     def __str__(self):
@@ -30,5 +36,6 @@ class CycleCommand(Command):
 
 
 class Url(GenericAttachment):
-    def __init__(self, url):
+    def __init__(self, url, alias=None):
+        super().__init__(alias)
         self.url = url
