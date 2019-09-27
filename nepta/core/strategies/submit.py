7f0e5c171498e2e06223110f9b7cd1461014bb2f
Adrian Tomasov <atomasov@redhat.com>
2019-09-27 12:43:43 +0200

    [strategy/submit] adding ReliableSubmit strategy, which tries to send data with rsync several times

--- nepta/core/strategies/submit.py
+++ nepta/core/strategies/submit.py
@@ -1,4 +1,8 @@
 import logging
+import jinja2
+
+from multiprocessing import Pool
+from time import sleep
 
 from nepta.core.strategies.generic import Strategy
 from nepta.core.distribution.components import Command
@@ -20,7 +24,7 @@ class Submit(Strategy):
     def submit(self):
         logger.info('Starting rsync results')
         for rsync in self.configuration.get_subset(m_class=RsyncHost):
-            logger.info('rsyncing results to %s', ":".join([rsync.server, rsync.destination]))
+            logger.info('Rsyncing results to %s', ":".join([rsync.server, rsync.destination]))
             c = Command(self._RSYNC_TEMPLATE.render(path=self.package.path, rsync=rsync))
             c.run()
             out, ret = c.watch_output()
@@ -29,3 +33,38 @@ class Submit(Strategy):
             else:
                 logger.error("Rsync failed")
                 logger.error(out)
+
+
+class ReliableSubmit(Submit):
+
+    @staticmethod
+    def _rsync_sender(rsync: RsyncHost, cmd: str):
+        for delay in rsync.attempt_delays:
+            sleep(delay * 60)
+            dest = ":".join([rsync.server, rsync.destination])
+            logger.info(f'Rsyncing results to {dest}', )
+
+            c = Command(cmd)
+            c.run()
+            out, ret = c.get_output()
+            logger.debug(out)
+            if ret == 0:
+                logger.info(f"Rsync result to {dest} >> Successful")
+                break
+            else:
+                logger.error(f"Rsync result to {dest} >> Failed")
+        else:  # no break
+            logger.error(f'All rsync attempts to destination {rsync.destination} failed.'
+                         f' Result was not sent to data server.')
+
+    @Strategy.schedule
+    def submit(self):
+
+        logger.info('Starting reliable rsyncing results to servers')
+        rsyncs = list(self.configuration.get_subset(m_class=RsyncHost))
+
+        with Pool(len(rsyncs)) as p:
+            p.starmap(ReliableSubmit._rsync_sender,
+                      [(rsync, self._RSYNC_TEMPLATE.render(rsync=rsync, path=self.package.path)) for rsync in rsyncs])
+
+        logging.debug('Quitting reliable rsyncing results to servers')
