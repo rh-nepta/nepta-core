b0eb5f0f3f4092e843771b9c41ae5dd1cbc4111b
Adrian Tomasov <atomasov@redhat.com>
2020-03-24 13:22:44 +0100

    [test] adding more tests

--- unittests/model/system_test.py
+++ unittests/model/system_test.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from nepta.core.model import system, bundles
+from nepta.core.distribution.conf_files import KernelModuleOptions
 
 
 class ModelTest(TestCase):
@@ -18,3 +19,13 @@ class ModelTest(TestCase):
         b.cryptd = cryptd
 
         self.assertEqual(len(b.get_subset(m_class=system.KernelModule)), 2)
+
+    def test_kernel_module_conf_files(self):
+        b = bundles.Bundle()
+        b.chcr = system.KernelModule('chcr')
+        b.cryptd = system.KernelModule('cryptd', cryptd_max_cpu_qlen=2048)
+
+        self.assertEqual(KernelModuleOptions(b.chcr).get_content(), '')
+        self.assertEqual(KernelModuleOptions(b.chcr).get_path(), '/etc/modprobe.d/chcr.conf')
+        self.assertEqual(KernelModuleOptions(b.cryptd).get_content(), 'options cryptd_max_cpu_qlen=2048\n')
+
