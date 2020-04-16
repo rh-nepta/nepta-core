a7588dc1e86f8be7f865549d0f33c2cf2885a49f
Adrian Tomasov <atomasov@redhat.com>
2020-04-16 18:00:08 +0200

    [strategies] report: del TODO -> not gonna happen

--- nepta/core/strategies/report.py
+++ nepta/core/strategies/report.py
@@ -16,6 +16,5 @@ class Report(Strategy):
     @Strategy.schedule
     def report(self):
         logger.info('reporting results to beaker')
-        # TODO: manager should return all results and attachments packed in a tgz archive and return here its path
         Rstrnt.submit_log(self.package.metas._xml_file.path)
         Rstrnt.report_result(success=self.success, filename=self.package.store.path)
