85935d8d7b5af6ee837248912aadd3f270f8e3bb
Adrian Tomasov <atomasov@redhat.com>
2020-05-22 11:02:54 +0200

    [test] add type checking test

--- unittests/model/network_test.py
+++ unittests/model/network_test.py
@@ -3,6 +3,7 @@ import ipaddress as ia
 
 from nepta.core.model.network import NetperfNet4, NetperfNet6
 from nepta.core.model.network import IPv4Configuration, IPv6Configuration
+from nepta.core.model import network
 
 
 class NetFormatterTest(TestCase):
@@ -40,3 +41,46 @@ class NetFormatterTest(TestCase):
                 self.assertEqual(ip, cfg4[j])
             for j, ip in enumerate(cfg6):
                 self.assertEqual(ip, cfg6[j])
+
+
+class InterfaceTest(TestCase):
+    def setUp(self) -> None:
+        self.net4 = NetperfNet4(ia.ip_network('192.168.0.0/24'))
+        self.gw4 = self.net4.broadcast_address
+        self.net6 = NetperfNet6(ia.ip_network('FE80::/64'))
+        self.dns6 = self.net6.new_addresses(3)
+
+    def test_init_and_str(self):
+        """
+        This test is not used primarily to raise assertions. It is used to check accepted variables by constructor. The
+        check should be evaluated using `mypy`.
+        """
+        generic = network.Interface(
+            'eth2',
+            IPv4Configuration(
+                self.net4.new_addresses(3),
+                self.gw4
+            ),
+            IPv6Configuration(
+                self.net6.new_addresses(2),
+                dns=self.dns6
+            )
+        )
+
+        mac = '00:11:22:33:44:55:66'
+        eth = network.EthernetInterface(
+            'eth2',
+            mac,
+            IPv4Configuration(
+                self.net4.new_addresses(3),
+                self.gw4
+            ),
+            IPv6Configuration(
+                self.net6.new_addresses(2),
+                dns=self.dns6
+            )
+        )
+        self.assertEqual(eth.mac, mac)
+
+        vlan1 = network.VlanInterface(generic, 10, self.net4.new_config(2))
+        vlan2 = network.VlanInterface(eth, 20)
