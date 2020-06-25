6d02c0a20d8958cbc7f57b5d871047b90f051567
Adrian Tomasov <atomasov@redhat.com>
2020-06-25 17:40:38 +0200

    [strategy] report aggregates result of all scenarios

--- nepta/core/strategies/report.py
+++ nepta/core/strategies/report.py
@@ -1,20 +1,26 @@
 import logging
 
-from nepta.core.strategies.generic import Strategy
+from nepta.core.strategies.generic import Strategy, CompoundStrategy
+from nepta.core.strategies.run import RunScenarios
 from nepta.core.distribution.utils.rstrnt import Rstrnt
 
 logger = logging.getLogger(__name__)
 
 
 class Report(Strategy):
-    def __init__(self, package, success):
+    def __init__(self, package, all_strategies: CompoundStrategy = None, result: bool = True):
         super().__init__()
         self.package = package
-        self.success = success
+        self.all_strategies = all_strategies
+        self.result = result
 
     @Strategy.schedule
     def report(self):
+        if self.all_strategies:
+            for strategy in self.all_strategies.strategies:
+                if isinstance(strategy, RunScenarios):
+                    self.result &= strategy.aggregated_result
         logger.info('reporting results to beaker')
         Rstrnt.submit_log(self.package.metas._xml_file.path)
         Rstrnt.submit_log(self.package.store.path)
-        Rstrnt.report_result(success=self.success)
+        Rstrnt.report_result(success=self.result)
