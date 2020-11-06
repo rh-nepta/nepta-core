69034a17ad0f94c79922092a0cf258f85e17f71d
Adrian Tomasov <atomasov@redhat.com>
2020-11-06 11:41:49 +0100

    [attachment] fixing unsaved attr

--- nepta/core/model/attachments.py
+++ nepta/core/model/attachments.py
@@ -1,5 +1,8 @@
+from nepta.dataformat import Compression
+
+
 class GenericAttachment(object):
-    def __init__(self, alias=None, compression=None):
+    def __init__(self, alias=None, compression=Compression.NONE):
         self.alias = alias
         self.compression = compression
 
@@ -8,26 +11,26 @@ class GenericAttachment(object):
 
 
 class File(GenericAttachment):
-    def __init__(self, file_path, alias=None, compression=None):
+    def __init__(self, file_path, alias=None, compression=Compression.NONE):
         super().__init__(alias, compression)
         self.f_path = file_path
 
 
 class Directory(GenericAttachment):
-    def __init__(self, directory_path, alias=None, compression=None):
+    def __init__(self, directory_path, alias=None, compression=Compression.NONE):
         super().__init__(alias, compression)
         self.d_path = directory_path
 
 
 class Command(GenericAttachment):
-    def __init__(self, cmdline, alias=None, compression=None):
+    def __init__(self, cmdline, alias=None, compression=Compression.NONE):
         super().__init__(alias, compression)
         self.cmdline = cmdline
 
 
 class CycleCommand(Command):
-    def __init__(self, cmd_for_list, cmdline, alias=None, compression=None):
-        super().__init__(alias, compression)
+    def __init__(self, cmd_for_list, cmdline, alias=None, compression=Compression.NONE):
+        super().__init__(cmdline, alias, compression)
         self.cmd_for_list = cmd_for_list
 
     def __str__(self):
