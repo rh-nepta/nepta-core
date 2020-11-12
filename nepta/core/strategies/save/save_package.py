cd16faf798b4c1987e62df8eae451387f4fcfb6a
Adrian Tomasov <atomasov@redhat.com>
2020-11-12 11:31:21 +0100

    [strategies] package: add strategy for open and close rem packages

--- nepta/core/strategies/save/save_package.py
+++ nepta/core/strategies/save/save_package.py
@@ -7,12 +7,15 @@ from nepta.dataformat import DataPackage
 logger = logging.getLogger(__name__)
 
 
-class Save(Strategy):
-    SYMLINK_NAME = '/root/result'
+class PackagesStrategy(Strategy):
 
     def __init__(self, package: DataPackage):
         super().__init__()
         self.package = package
+
+
+class Save(PackagesStrategy):
+    SYMLINK_NAME = '/root/result'
 
     @Strategy.schedule
     def save(self):
@@ -25,3 +28,17 @@ class Save(Strategy):
             os.unlink(self.SYMLINK_NAME)
         logger.info(f'Creating symlink >> {self.SYMLINK_NAME} -> {pkg_path}')
         os.symlink(pkg_path, self.SYMLINK_NAME)
+
+
+class OpenRemotePackages(PackagesStrategy):
+
+    @Strategy.schedule
+    def save_remote_packages(self):
+        self.package.remote_packages.unarchive()
+
+
+class SaveRemotePackages(PackagesStrategy):
+
+    @Strategy.schedule
+    def save_remote_packages(self):
+        self.package.remote_packages.save()
