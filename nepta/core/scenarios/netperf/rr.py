5fc5e9d66b33bc24e5cddac348105be315512244
Adrian Tomasov <atomasov@redhat.com>
2021-04-29 18:39:16 +0200

    [scenarios] netperf: fix typo

--- nepta/core/scenarios/netperf/rr.py
+++ nepta/core/scenarios/netperf/rr.py
@@ -22,7 +22,7 @@ class NetperfTcpRr(SingleStreamGeneric):
         )
         if cpu_pinning:
             netperf_test.local_cpu = cpu_pinning[0][0]
-            netperf_test.remote_cpu = cpu_pinningi[0][1]
+            netperf_test.remote_cpu = cpu_pinning[0][1]
         return netperf_test
 
     def parse_results(self, test):
