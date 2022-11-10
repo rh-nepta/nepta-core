9146e19284221d57b32674db2f1554595dab2c03
Adrian Tomasov <atomasov@redhat.com>
2022-11-10 09:10:38 +0100

    [strategies] prepare: handle missing config

--- nepta/core/strategies/prepare.py
+++ nepta/core/strategies/prepare.py
@@ -42,7 +42,10 @@ class Prepare(Strategy):
         remote_scenarios = model.bundles.Bundle()
         for host in sync_objs:
             host_conf = model.bundles.HostBundle.find(host.hostname, self.conf.conf_name)
-            remote_scenarios += host_conf.get_subset(m_class=GenericIPerf3Stream)
+            if host_conf:
+                remote_scenarios += host_conf.get_subset(m_class=GenericIPerf3Stream)
+            else:
+                logger.error(f'Synchronized host {host} does not have configuration for current testcase.')
 
         # spawn at least 100 iPerf3 instances due to laziness
         max_iperf3_instances = 100
