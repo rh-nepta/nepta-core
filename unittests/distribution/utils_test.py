c7b763dfe6a3ae111346ea91c377e2373f8799f8
Adrian Tomasov <atomasov@redhat.com>
2022-12-21 16:29:10 +0100

    [tests] skip bare metal tests

--- unittests/distribution/utils_test.py
+++ unittests/distribution/utils_test.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from nepta.core.distribution.utils.system import TimeDateCtl
 from nepta.core.model.system import TimeZone
@@ -6,11 +6,14 @@ from nepta.core.model.system import TimeZone
 
 class TimeDateCtlTest(TestCase):
 
+    @skip('skip for CI')
     def test_status(self):
         out = TimeDateCtl.status()
         self.assertTrue('Time zone' in out)
         self.assertTrue('NTP' in out)
 
+    @skip('skip for CI')
+    def test_status(self):
     def test_set_timezone(self):
         zone = TimeZone('Europe/Prague')
         out = TimeDateCtl.set_timezone(zone)
