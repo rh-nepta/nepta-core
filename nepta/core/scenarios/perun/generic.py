d3d2ee16de46749b7d5211d48517c5d64d6729c4
Adrian Tomasov <atomasov@redhat.com>
2025-06-16 15:46:40 +0200

    [perun] remove commented-out perf data folding code + add return

--- nepta/core/scenarios/perun/generic.py
+++ nepta/core/scenarios/perun/generic.py
@@ -18,16 +18,7 @@ class PerunBpfMixin(StreamGeneric):
 
     def run_scenario(self):
         pathlib.Path(self.perun_directory).mkdir(parents=True, exist_ok=True)
-        results = super().run_scenario()
-
-        # # fold perf data
-        # logger.info('Folding perf data')
-        # for perf_file in pathlib.Path(self.perun_directory).rglob('*.perf.data'):
-        #     logger.debug(f'Folding {perf_file}')
-        #     Perf.fold_output(perf_file, f"{perf_file}.folded")
-        #     logger.debug(f'Deleting {perf_file}!')
-        #     os.remove(perf_file)
-        # return results
+        return super().run_scenario()
 
 
 class PerunPerfMixin(PerunBpfMixin):
